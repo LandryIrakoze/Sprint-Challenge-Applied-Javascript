925f484d5e6ca8dc2d4c8d0b766f57c47e66abc0
Landry Irakoze <irakozel@gmail.com>
2019-06-21 12:31:57 -0400

    fixed classlist remove, removed period

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,7 +4,7 @@ class TabLink {
     this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
-    this.tabData = this.tabElement.data; 
+    this.tabData = this.tabElement.dataset.tab; 
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
@@ -27,13 +27,12 @@ class TabLink {
   }
 
   selectTab(){
-
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll('.tab');
-    
-    // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach(tab => tab.classList.remove('.active-tab'));
 
+    // Iterate through the NodeList removing the .active-tab class from each element
+    Array.from(tabs).forEach(tab => tab.classList.remove('active-tab'));
+    
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll('.card');
 
