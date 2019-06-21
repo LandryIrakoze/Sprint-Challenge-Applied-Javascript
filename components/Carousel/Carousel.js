49bd8f07c6272bb623ceb89f74aa9e6e46135aa8
Landry Irakoze <irakozel@gmail.com>
2019-06-21 13:24:23 -0400

    completed test functionality for carousel array

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,28 @@
 class Carousel {
+    constructor(element) {
+        this.element = element;
+        
+        this.leftButton = this.element.querySelector('.left-button');
+        this.rightButton = this.element.querySelector('.right-button');
 
+        this.leftButton.addEventListener('click', () => this.slideImages());
+        this.rightButton.addEventListener('click', () => this.slideImages());
+
+        // this.test = [1, 2, 3, 4]
+    }
+    
+    slideImages() {
+        let images = Array.from(this.element.querySelectorAll('img'));
+        
+        console.log(this.test);
+        this.test.unshift(this.test[this.test.length -1]);
+        this.test.pop();
+        console.log(this.test);
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+let myCarousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
@@ -12,3 +32,6 @@ let carousel = document.querySelector();
     5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+/* Create an 'infinite loop' carousel */
+/* In which as long as you click on an arrow, the array of images will loop over itself. */
